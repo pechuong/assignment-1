2559745193f977feba0524b4abaf8919fd6b9548
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-10-17 20:13:35 -0700

    Removed the try catches and the sysouts

--- HybridSorting.java
+++ HybridSorting.java
@@ -9,16 +9,8 @@ public class HybridSorting implements SortingAlgorithm {
 
 	public static void sort(int[] arr, int run_size) {
 		ArrayList<Integer> runIndex = findRuns(arr, run_size);	
-		//System.out.println("Run Index: " + runIndex);
-		//System.out.println("Array atm: " + Arrays.toString(arr));
 		ArrayList<Integer> newRuns = sortNonRuns(arr, runIndex, run_size);	
-		//System.out.println("Array after nonRunsSort: " + Arrays.toString(arr));
-		//System.out.println("Old Runs: " + runIndex);
-		//System.out.println("New Runs: " + newRuns);
-		//System.out.println("Starting Array: " + Arrays.toString(arr));
-		//mergeSort(arr, newRuns, newRuns.get(0), newRuns.size() - 1);
 		mergeSort(arr, newRuns);
-		//System.out.println("Ending Array: " + Arrays.toString(arr));
 	}
 	
 	/**
@@ -56,47 +48,35 @@ public class HybridSorting implements SortingAlgorithm {
 		}	
 		ArrayList<Integer> newIndexes = new ArrayList<>();
 		newIndexes.addAll(runIndexes);
-		try {
-			if (runIndexes.get(0) != 0) {
-				QuickSort.sort(arr, 0, runIndexes.get(0) -1);
-				newIndexes.add(0, 0);
-			}
-			for (int i = 1; i < runIndexes.size() - 1; i++) {
-				QuickSort.sort(arr, runIndexes.get(i) + run_size, runIndexes.get(i + 1) - 1); 
-				newIndexes.add(i + 2, runIndexes.get(i) + run_size);
-			}
-		} catch (Exception e) {
-			System.out.println("Error in sortNonRuns");
-			System.out.println(e);
+		if (runIndexes.get(0) != 0) {
+			QuickSort.sort(arr, 0, runIndexes.get(0) -1);
+			newIndexes.add(0, 0);
+		}
+		for (int i = 1; i < runIndexes.size() - 1; i++) {
+			QuickSort.sort(arr, runIndexes.get(i) + run_size, runIndexes.get(i + 1) - 1); 
+			newIndexes.add(i + 2, runIndexes.get(i) + run_size);
 		}
 		return newIndexes;
 	}
 	
 	public static void mergeSort(int[] arr, ArrayList<Integer> runIndex) {
-		try {
-			if (runIndex.isEmpty()) {
-				return;
-			}
-			while (runIndex.size() > 2) {
-				for (int i = 0; i < runIndex.size() - 1; i++) {
-					merge(arr, runIndex.get(i), runIndex.get(i + 1), runIndex.get(i + 1), runIndex.get(i + 2));
-					runIndex.remove(i + 1);
-					i++;
-				}
-			}
-			merge(arr, runIndex.get(0), runIndex.get(1), runIndex.get(1), arr.length);
-		} catch (Exception e) {
-			System.out.println("Error merging runs back");
-			System.out.println(e);
+		if (runIndex.isEmpty()) {
+			return;
 		}
+		while (runIndex.size() > 2) {
+			for (int i = 0; i < runIndex.size() - 1; i++) {
+				merge(arr, runIndex.get(i), runIndex.get(i + 1), runIndex.get(i + 1), runIndex.get(i + 2));
+				runIndex.remove(i + 1);
+				i++;
+			}
+		}
+		merge(arr, runIndex.get(0), runIndex.get(1), runIndex.get(1), arr.length);
 	}
 	public static void merge(int[] arr, int left, int leftEnd, int right, int rightEnd) {
 		merge(left, Arrays.copyOfRange(arr, left, leftEnd), Arrays.copyOfRange(arr, right, rightEnd), arr);
 	}
 	public static void merge(int left, int[] leftArr, int[] rightArr, int[] a) {
 		int leftCount = 0, rightCount = 0, target = left;
-		System.out.println("Left Arr: " + Arrays.toString(leftArr));
-		System.out.println("Right Arr: " + Arrays.toString(rightArr));
 		while (leftCount < leftArr.length && rightCount < rightArr.length) {
 			if (leftArr[leftCount] <= rightArr[rightCount]) {
 				a[target++] = leftArr[leftCount++];
@@ -112,6 +92,7 @@ public class HybridSorting implements SortingAlgorithm {
 			a[target++] = rightArr[rightCount++];
 		}
 
+	
 	}
 
 }
